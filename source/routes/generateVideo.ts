845ebdbe91d213d901145cad0ed949d6017dff60
Larry <lauris.skraucis@gmail.com>
2021-05-14 10:16:04 +0200

    Removed unused controller & route

--- source/routes/generateVideo.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import controller from '../controllers/generateVideo';
-
-const router = express.Router();
-
-router.post('/videos/generate', controller.generateVideo);
-
-export = router;
